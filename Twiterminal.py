a27193baefde14573578e14107983b2f3c10010e
Ibrahim Alghamdi <ibrahim.a.g.94@gmail.com>
2012-05-24 01:21:12 -0700

    Added some windows commands.

--- Twiterminal.py
+++ Twiterminal.py
@@ -13,7 +13,7 @@ import tweepy, webbrowser, time, os, subprocess, random, sys
 
 # Contains all the system commands.
 class Functions:
-    
+
     # Initial method.
     def __init__(self):
 
@@ -46,94 +46,81 @@ class Functions:
 
         # Windows OS:
         elif self.OS == "windows":
-            pass
-<<<<<<< HEAD
-        
-=======
+            process = subprocess.Popen("shutdown -s", shell=True)
+            process.wait()
 
->>>>>>> 0febe2f... Some changes.
         # Unsupported OS's:
         else:
-            print "Operating system is not supported." 
+            print "Operating system is not supported."
 
     # Restart method.
     def restart(self):
+
         if self.OS == "unix":
-            process = subprocess.Popen("sudo shutdown -r now", shell=True)
+            process = subprocess.Popen("sudo shutdown -s now", shell=True)
             process.wait()
 
         elif self.OS == "windows":
-            pass
-<<<<<<< HEAD
-        
-=======
+            process = subprocess.Popen("sudo shutdown -r now", shell=True)
+            process.wait()
 
->>>>>>> 0febe2f... Some changes.
         else:
-            print "Operating system is not supported." 
+            print "Operating system is not supported."
 
     # Sleep/Suspend method.
     def sleep(self):
+
         if self.OS == "unix":
             process = subprocess.Popen("sudo pm-suspend", shell=True)
             process.wait()
 
         elif self.OS == "windows":
-            pass
-<<<<<<< HEAD
-        
-=======
+            process = subprocess.Popen("rundll32.exe powrprof.dll,SetSuspendState 0,1,0", shell=True)
+            process.wait()
+#pass
 
->>>>>>> 0febe2f... Some changes.
         else:
-            print "Operating system is not supported." 
+            print "Operating system is not supported."
 
     # Hibernate method.
     def hibernate(self):
+
         if self.OS == "unix":
             process = subprocess.Popen("sudo pm-hibernate", shell=True)
             process.wait()
 
         elif self.OS == "windows":
-            pass
-<<<<<<< HEAD
-        
-=======
+            process = subprocess.Popen("rundll32.exe powrprof.dll,SetSuspendState", shell=True)
+            process.wait()
+#pass
 
->>>>>>> 0febe2f... Some changes.
         else:
-            print "Operating system is not supported." 
+            print "Operating system is not supported."
 
     # Logoff method.
     def logoff(self):
+
         if self.OS == "unix":
             process = subprocess.Popen("logout", shell=True)
             process.wait()
 
         elif self.OS == "windows":
             pass
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> 0febe2f... Some changes.
         else:
-            print "Operating system is not supported." 
+            print "Operating system is not supported."
 
     # System information method.
     def status(self):
+
         if self.OS == "unix":
             pass #subprocess.Popen("", shell=True)
-<<<<<<< HEAD
-            
-=======
 
->>>>>>> 0febe2f... Some changes.
         elif self.OS == "windows":
             pass
-             
+
         else:
-            print "Operating system is not supported." 
+            print "Operating system is not supported."
 
     # Custom command method.
     def customCommand(self, command):
@@ -148,18 +135,6 @@ class Functions:
         elif self.OS == "windows":
             process = subprocess.Popen(command, shell=True)
             process.wait()
-            return process
-
-        else:
-            print "Operating system is not supported." 
-
-# Contains all the Twitter functions.
-class Stream:
-
-    # Initial method.
-    def __init__(self):
-
-        # Imports settings.
             return process
 
         else:
@@ -188,25 +163,28 @@ class Stream:
 
     # Reply method.
     def replyTweet(self, tweet, statusID):
-        
+
         # Replies to a specific tweet using its status id.
-        self.api.update_status("@%s %s %d" %(self.username, tweet, config.randomNumber()), in_reply_to_status_id = statusID)
+        self.api.update_status("@%s %s %d" %(self.username,\
+                                           tweet,\
+                                           config.randomNumber()),\
+                                           in_reply_to_status_id = statusID)
 
     # Find command method. Finds the command
     # within a tweet.
     def findCommand(self, status, statusID):
-       
-        # Strips away the client's accound name and command. 
+
+        # Strips away the client's accound name and command.
         command = status.strip("@%s %s " %(self.me, self.command))
 
         # Determines whether it's a custom command
         # or a pre-made command.
         # If a custom command:
-        if command.startswith("\"") == True: 
-            
+        if command.startswith("\"") == True:
+
             # Splits ".
             command = command.split("\"")
-            
+
             # Runs the custom command method and sets
             # its output to a variable <process>.
             process = functions.customCommand(command[1])
@@ -215,58 +193,58 @@ class Stream:
             # reply with "No, sir!":
             if process.poll() != 0:
                 self.replyTweet("No, sir!", statusID)
-                
+
             # Otherwise reply with "Yes, sir!":
             else:
                 self.replyTweet("Yes, sir!", statusID)
-         
+
         #If a pre-made command:
         else:
             # Find the command in the status and
             # run the appropriate function:
             if "shutdown" in command.lower():
                 functions.shutdown()
-          
+
             elif "restart" in command.lower():
                 functions.restart()
-            
+
             elif "sleep" in command.lower():
                 functions.sleep()
-           
+
             elif "hibernate" in command.lower():
                 functions.hibernate()
-          
+
             elif "logoff" in command.lower():
                 functions.logoff()
-            
+
             elif "status" in command.lower():
                 functions.status()
-            
+
             # Replies with "Command not found, sir!" when
             # command is not found:
             else:
                 self.replyTweet("Command not found, sir!", statusID)
                 print "Command not found."
-    
+
     # Import mentions method.
     def streamMentions(self):
-        
+
         # Sets GMT time to a variable.
         self.date = config.date()
-       
+
         # While-loop.
         while True:
             try:
-                
+
                 # Clears screen.
                 config.clearScreen()
                 print "Watcher (Ctrl + C to quit)"
-                
+
                 # Initiates lists.
                 status_list = [] # Contains statuses.
                 status_date = [] # Contains statuses dates.
-                status_id   = [] # Contains statuses ID's. 
-               
+                status_id   = [] # Contains statuses ID's.
+
                 # Imports mentions and sets them to a variable.
                 statuses = tweepy.Cursor(self.api.mentions).items()
 
@@ -286,7 +264,7 @@ class Stream:
                         status_list.append(status.text) # Statuses.
                         status_date.append(str(status.created_at)) # Statuses dates.
                         status_id.append(status.id) # Statuses ID's.
-                
+
                 # Reverses lists to be from older to newer.
                 status_list.reverse() # Statuses.
                 status_date.reverse() # Statuses dates.
@@ -302,23 +280,23 @@ class Stream:
                     # Prints status and status date/time.
                     print "\t\n[%s] - %s\n" %(status_date[counter],\
                                               status_list[counter])
-                    
+
                     # Sleeps for 2 seconds.
                     time.sleep(2)
 
                     # Passes status and status ID to findCommand method.
                     self.findCommand(status_list[counter],\
                                       status_id[counter])
-                    
+
                     # Sleeps for 2 seconds.
                     time.sleep(2)
-                    
+
                     # Adds 1 to counter.
                     counter += 1
 
                 # Important command. This sets the date variable
                 # equal to the last executed command's status so
-                # the program will ignore that status and 
+                # the program will ignore that status and
                 # statuses before that one.
                 self.date = status_date[-1]
 
@@ -330,24 +308,24 @@ class Stream:
                 return False
             # Passes when IndexError is raised.
             except IndexError:
-                pass 
+                pass
 
 # Contains all the settings and access information.
 class Config:
 
     # Checker method. Checks if ".settings" file exists.
     def checker(self):
-        
+
         # Clears screen.
         config.clearScreen()
 
         # If ".settings" file doesn't exist:
         if os.path.exists(".settings") == False:
-            
+
             # Prints a message and sleeps for 6 seconds.
             print "You need to go to the settings menu before proceeding to this step."
             time.sleep(6)
-    
+
         # If ".settings" file exists:
         else:
             # Runs streamMentions method.
@@ -355,8 +333,8 @@ class Config:
 
     # Clear screen method.
     def clearScreen(self):
-       
-        # If OS is Linux, Mac, or BSD: 
+
+        # If OS is Linux, Mac, or BSD:
         if sys.platform.startswith("linux")\
             or sys.platform.startswith("darwin")\
             or sys.platform.startswith("freebsd"):
@@ -366,16 +344,16 @@ class Config:
 
         # If OS is Windows:
         elif sys.platform.startswith("win32"):
-	        
+
             # Executes <clrscr>.
             os.system("clrscr")
 
     # OAuth method.
     def OAuth(self):
-        
+
         # If ".consumer" file exists:
         if os.path.exists(".consumer"):
-            
+
             # Opens file and import information.
             f = open(".consumer", "r")
             info = f.read()
@@ -387,7 +365,7 @@ class Config:
             # Sets data to variables.
             consumer_key = info[0] # Consumer Key.
             consumer_secret = info[1] # Consumer Secret.
-            
+
         # If ".consumer" doesn't exist:
         else:
 
@@ -400,16 +378,16 @@ class Config:
             # Prompts user for Consumer Key and Consumer Secret.
             consumer_key = str(raw_input("Enter consumer key: ")).strip()
             consumer_secret = str(raw_input("Enter consumer secret: ")).strip()
-            
+
             # Writes Consumer Key and Consumer Secret
             # to a file for later use.
             f = open(".consumer", "w")
             f.writelines(consumer_key + "," + consumer_secret)
             f.close()
-        
+
         # If ".access" file exists:
         if os.path.exists(".access"):
-            
+
             # Opens file and imports information.
             f = open(".access", "r")
             info = f.read()
@@ -442,15 +420,15 @@ class Config:
             auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
             redirect_url = auth.get_authorization_url()
             webbrowser.open_new_tab(redirect_url)
-    
+
             # Sleep for 5 seconds.
             time.sleep(5)
-   
+
             # Prompts the user for PIN to have access to the
-            # client's Twitter account. 
+            # client's Twitter account.
             verifier = str(raw_input("Enter PIN: ")).strip()
             auth.get_access_token(verifier)
-    
+
             # Sets Access Key and Access Secret to variables.
             access_key = auth.access_token.key
             access_secret = auth.access_token.secret
@@ -469,10 +447,10 @@ class Config:
     # Date method. Returns GMT time in the same
     # format as Twitter.
     def date(self):
-        
+
         # Gets GMT time.
         gmttime = time.asctime(time.gmtime(time.time()))
-        
+
         # Splits information seperated by " ".
         gmttime = gmttime.split(" ")
 
@@ -499,18 +477,18 @@ class Config:
                                gmttime[2],\
                                gmttime[3])
 
-    # Random number generator method. Used for 
+    # Random number generator method. Used for
     # avoiding double tweets and for user confirmation.
     def randomNumber(self):
-       
-        # Generates a random number and returns it. 
-        num = int(random.random() * pow(10,6)) 
+
+        # Generates a random number and returns it.
+        num = int(random.random() * pow(10,6))
         return num
 
     # Settings method. This is where settings will
     # be made and edited by the user.
     def settings(self):
-       
+
         # Clears screen.
         self.clearScreen()
 
@@ -552,17 +530,17 @@ class Config:
             user = int(raw_input("Enter a value: ").strip())
             info.pop(0)
             info.insert(0, str(user))
-        
+
         elif user == "2":
             user = str(raw_input("Enter a starting command: ").strip())
             info.pop(1)
             info.insert(1, user)
-        
+
         elif user == "3":
             user = str(raw_input("Enter a username: ").strip().strip("@"))
             info.pop(2)
             info.insert(2, user)
-        
+
         # If user chooses 4 OR 5 OR 6:
         # 5 - Delete settings.
         # 6 - Delete login information.
@@ -571,12 +549,12 @@ class Config:
             # Generate a random number for confirmation.
             num = self.randomNumber()
             user = str(raw_input("Please enter %s to delete settings (Enter anything to keep them):" %num))
-           
+
             # If user enters number right:
             if user == str(num):
 
                 try:
-                    # Removes ".settings" file, prints a 
+                    # Removes ".settings" file, prints a
                     # message and sleeps for 3 seconds.
                     os.remove(".settings")
                     print "Settings have been successfully removed."
@@ -584,7 +562,7 @@ class Config:
 
                 except:
                     print "Unexpected error."
-<<<<<<< HEAD
+
                     time.sleep(3)
 
             # If user enters number wrong:
@@ -596,9 +574,7 @@ class Config:
         elif user == "5":
             num = self.randomNumber()
             user = str(raw_input("Please enter %s to reset login information (Enter anything to keep them):" %num))
-           
-=======
->>>>>>> 0febe2f... Some changes.
+
             if user == str(num):
                 try:
                     os.remove(".consumer")
@@ -615,7 +591,7 @@ class Config:
         elif user == "6":
             num = self.randomNumber()
             user = str(raw_input("Please enter %s to reset all settings (Enter anything to keep everything):" %num))
-           
+
             if user == str(num):
                 try:
                     os.remove(".settings")
@@ -629,11 +605,11 @@ class Config:
             else:
                 print "Nothing has been changed."
                 time.sleep(3)
-        
+
         # Quits if user enters -1.
         elif user == "-1":
-            pass 
-        
+            pass
+
         # If user enters an invalid input:
         else:
             # Prints a message and sleeps for 2 seconds.
@@ -650,8 +626,8 @@ class Config:
 
 # Main method
 def main():
-   
-    # Config global variable. 
+
+    # Config global variable.
     global config
     config = Config()
 
@@ -687,13 +663,13 @@ def main():
 
             elif user == 2:
                 config.settings()
-            
+
             elif user == 3:
                 pass
-            
+
             elif user == 4:
                 pass
-            
+
             elif user == 5:
                 # Prints a message, sleeps for 2 seconds,
                 # clears screen, and returns false to break
@@ -702,13 +678,13 @@ def main():
                 time.sleep(2)
                 config.clearScreen()
                 return False
-            
+
             else:
-                # Prints a message and sleeps for 2 seconds 
+                # Prints a message and sleeps for 2 seconds
                 # when user enters an invalid input.
                 print "Invalid input."
                 time.sleep(2)
-    
+
     # Closes program when user hits <Ctrl + C>
     except KeyboardInterrupt:
         print "\n"
